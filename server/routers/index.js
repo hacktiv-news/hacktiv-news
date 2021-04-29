fcad11f1a8c140ad4c875a720d7781dc97a4e619
ericksonmanurung <erickson.manurung23@gmail.com>
2021-04-30 00:36:45 +0700

    tambah news health dan sport

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 const routesUser = require('./user')
 const axios = require('axios')
 
+//3rd Party API News
+// all news
 router.get('/news', (req, res, next) => {
   axios
     .get(
-      `https://newsapi.org/v2/top-headlines?category=sports&apiKey=${process.env.NEWS_API_KEY}`
+      `https://newsapi.org/v2/top-headlines?country=id&apiKey=${process.env.NEWS_API_KEY}`
     )
     .then((data) => {
       res.status(200).json({ success: true, data: data.data.articles });
@@ -15,6 +17,37 @@ router.get('/news', (req, res, next) => {
       next(err);
     });
 });
+
+// sport news
+router.get('/news/sport', (req, res, next) => {
+  axios
+    .get(
+      `https://newsapi.org/v2/top-headlines?country=id&category=sport&apiKey=${process.env.NEWS_API_KEY}`
+    )
+    .then((data) => {
+      res.status(200).json({ success: true, data: data.data.articles });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+});
+
+// health news
+router.get('/news/health', (req, res, next) => {
+  axios
+    .get(
+      `https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=${process.env.NEWS_API_KEY}`
+    )
+    .then((data) => {
+      res.status(200).json({ success: true, data: data.data.articles });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+});
+//3rd Party API News
 
 router.get('/weather', (req, res, next) => {
   const kota = 'Jakarta'
