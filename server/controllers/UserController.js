1366f8975ac61db4d24f45935d62d13ed4f38c6b
ericksonmanurung <erickson.manurung23@gmail.com>
2021-04-30 09:29:26 +0700

    oauth google

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,7 @@
 const {User} = require('../models')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const {OAuth2Client} = require('google-auth-library');
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
@@ -48,6 +49,45 @@ class UserController{
             console.log(err)
             next(err)
         });
+    }
+
+
+    static googleLogin(req,res,next){
+        const client = new OAuth2Client(process.env.CLIENT_ID);
+        const token = req.body.token
+        async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        // const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+        console.log(payload)
+        User.findOne({
+            where:{
+                email:payload.email
+            }
+        })
+        .then((user) => {
+            if(!user){
+                return User.create({email:payload.email, password:process.env.DEFAULT_PASSWORD})
+            }else{
+                return user
+            }
+        })
+        .then((user) => {
+            const access_token = jwt.sign({id:user.id, email:user.email}, process.env.JWT_SECRET)
+            return res.status(200).json({access_token})
+        })
+        .catch((err) => {
+            next(err)
+        });
+        }
+        verify().catch(console.error);
     }
 }
 
--- server/routers/user.js
+++ server/routers/user.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const UserController = require('../controllers/UserController')
     
 router.post('/registrasi', UserController.regis)
-router.post('/login',UserController.login)
+router.post('/login', UserController.login)
+router.post('/googleLogin', UserController.googleLogin)
 
 module.exports = router
